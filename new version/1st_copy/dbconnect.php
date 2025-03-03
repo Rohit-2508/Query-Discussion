aea07b5238d75918b24171ad6a91690f87e80093
Rohit-2508 <109918640+Rohit-2508@users.noreply.github.com>
2025-03-04 02:03:41 +0530

    Final  updated version

---
+++ new version/1st_copy/dbconnect.php
@@ -0,0 +1,10 @@
+<?php
+$servername = "";
+$username = "";
+$password = "";
+$database = "";
+//details hidden for security purposes
+
+$conn = mysqli_connect($servername,$username,$password,$database);
+
+?>
